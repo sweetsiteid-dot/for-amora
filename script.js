9ff7fca299b0866fc5c9901585ca61265002a9fa
sweetsiteid-dot <sweetsite.id@gmail.com>
2026-08-17 19:38:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function checkPassword(){
 const password =
 document.getElementById("passwordInput").value;
 
-if(password === "1234"){
+if(password === "2608"){
 
 document.getElementById("passwordScreen")
 .style.display="none";
